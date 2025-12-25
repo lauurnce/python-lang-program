3251990a81a1a2b1311bf35cac094abfeb1e9e39
Lawrence Panes <paneslawrence8@gmail.com>
2025-12-25 17:20:45 +0800

    docs: while loop with nested for if-else example

--- inter-python-ex9.py
+++ inter-python-ex9.py
@@ -5,7 +5,7 @@ offset = -6
 while offset != 0 :
     print("correcting...")
     if offset > 0 :
-      offset - 1
+      offset = offset - 1
     else : 
-      offset + 1    
+      offset = offset + 1    
     print(offset)
