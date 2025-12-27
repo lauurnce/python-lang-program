0b52454b83702acc6099ae839d9ff5636dec3fc8
Lawrence Panes <paneslawrence8@gmail.com>
2025-12-28 00:43:38 +0800

    docs: sorting rows, subsetting columns, subsetting rows, adding new columns

--- data manipulation with pandas/py-ex2.py
+++ data manipulation with pandas/py-ex2.py
@@ -8,7 +8,7 @@ high_homelessness = homelessness[homelessness["indiv_per_10k"] > 20]
 high_homelessness_srt = high_homelessness.sort_values("indiv_per_10k", ascending=False)
 
 # From high_homelessness_srt, select the state and indiv_per_10k cols
-result = high_homelessness[["state", "indiv_per_10k"]]
+result = high_homelessness_srt[["state", "indiv_per_10k"]]
 
 # See the result
 print(result)
