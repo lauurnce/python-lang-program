d58f5a1987d7b88095be53dd4914b8fd04f4a078
Lawrence Panes <paneslawrence8@gmail.com>
2026-01-18 23:49:45 +0800

    docs: joining

---
+++ joining data with pandas/py-ex4.py
@@ -0,0 +1,6 @@
+# Create a custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg([iqr, "median"]))
