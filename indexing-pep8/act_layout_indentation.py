e8f1ce3380e3f57d95073a7c246e7851bbdd5419
Lawrence Panes <paneslawrence8@gmail.com>
2026-02-02 23:54:13 +0800

    docs: activities after the pep 8 workshop

---
+++ indexing-pep8/act_layout_indentation.py
@@ -0,0 +1,9 @@
+# Apply 79 Characters Limit, Indentation, Wrapping 
+# Item 1:
+def send_alert(message, user_id, timestamp, urgency_level, device_type, location_data):  
+    print("Alert sent!")
+
+# Item 2: 
+if score > 50:
+ print("You passed!") 
+ return True
---
+++ indexing-pep8/act_whitespace.py
@@ -0,0 +1,8 @@
+# Put a space around operators like 
+# Item 1:
+x=10
+final_score=x+50
+
+# Remove the space around default value 
+# Item 2:
+def create_profile(name, is_admin = False):  
