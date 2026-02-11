7b4e6b1b1ad28d62d42ce9c7216edf998d89ab42
Lawrence Panes <paneslawrence8@gmail.com>
2026-02-12 00:01:33 +0800

    docs: sorting

--- data manipulation with pandas/py-ex38.py
+++ data manipulation with pandas/py-ex38.py
@@ -5,4 +5,4 @@ airline_totals_sorted = airline_totals.sort_values("bumps_per_10k", ascending=Fa
 print(airline_totals_sorted)
 
 # Save as airline_totals_sorted.csv
-airline_totals_sorted.to_csv("airline_totals_sorted.csv"
+airline_totals_sorted.to_csv("airline_totals_sorted.csv")
