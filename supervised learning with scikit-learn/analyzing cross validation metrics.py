0956e19be94fd5c5d4d75a723eb9b7792bfb77c1
Lawrence Panes <paneslawrence8@gmail.com>
2026-02-17 02:15:00 +0800

    docs: day 2 - regression performance, cross validation, regularized regression

---
+++ supervised learning with scikit-learn/analyzing cross validation metrics.py
@@ -0,0 +1,9 @@
+# Print the mean
+import numpy as np
+print(np.mean(cv_results))
+
+# Print the standard deviation
+print(np.std(cv_results))
+
+# Print the 95% confidence interval
+print(np.quantile(cv_results, [0.025, 0.975]))
---
+++ supervised learning with scikit-learn/cross-validation for R-squared.py
@@ -0,0 +1,16 @@
+# Import the necessary modules
+import numpy as np
+from sklearn.model_selection import cross_val_score, KFold
+
+# Create a KFold object
+kf = KFold(n_splits=6, shuffle=True, random_state=5)
+
+reg = LinearRegression()
+
+# Compute 6-fold cross-validation scores
+cv_scores = cross_val_score(reg, X, y, cv=kf)
+
+# Print scores
+print(cv_scores)
+
+print(np.mean(cv_scores), np.std(cv_scores))
