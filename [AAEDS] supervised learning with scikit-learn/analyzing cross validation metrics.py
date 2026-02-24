c1966045cd087e7fae8d37d37147c0119720dc78
Lawrence Panes <paneslawrence8@gmail.com>
2026-02-24 23:20:17 +0800

    docs: update the variable name from cv_result to cv_results in the code snippet for analyzing cross validation metrics.py to match the variable used in the rest of the code.

--- [AAEDS] supervised learning with scikit-learn/analyzing cross validation metrics.py
+++ [AAEDS] supervised learning with scikit-learn/analyzing cross validation metrics.py
@@ -1,9 +1,9 @@
 # Print the mean
 import numpy as np
-print(np.mean(cv_result))
+print(np.mean(cv_results))
 
 # Print the standard deviation
-print(np.std(cv_result))
+print(np.std(cv_results))
 
 # Print the 95% confidence interval
-print(np.quantile(cv_result, [0.025, 0.975]))
+print(np.quantile(cv_results, [0.025, 0.975]))
