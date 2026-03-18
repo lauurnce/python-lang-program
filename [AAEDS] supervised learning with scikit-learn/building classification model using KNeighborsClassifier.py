f885f97e209c02193319fedf8eee954b13b20d27
Lawrence Panes <paneslawrence8@gmail.com>
2026-03-18 23:57:03 +0800

    docs: Update KNN classifier to use 5 neighbors instead of 6

--- [AAEDS] supervised learning with scikit-learn/building classification model using KNeighborsClassifier.py
+++ [AAEDS] supervised learning with scikit-learn/building classification model using KNeighborsClassifier.py
@@ -4,8 +4,8 @@ from sklearn.neighbors import KNeighborsClassifier
 y = churn_df["churn"].values
 X = churn_df[["account_length", "customer_service_calls"]].values
 
-# Create a KNN classifier with 6 neighbors
-knn = KNeighborsClassifier(n_neighbors=6)
+# Create a KNN classifier with 5 neighbors
+knn = KNeighborsClassifier(n_neighbors=5)
 
 # Fit the classifier to the data
 knn.fit(X, y)
