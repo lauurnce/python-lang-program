afb6ad2508d64608a33319089cc771905714666f
Lawrence Panes <paneslawrence8@gmail.com>
2026-03-29 23:53:22 +0800

    docs: Fix variable name in centering and scaling for classification.py

--- [AAEDS] supervised learning with scikit-learn/preprocessing and pipelines/centering and scaling for classification.py
+++ [AAEDS] supervised learning with scikit-learn/preprocessing and pipelines/centering and scaling for classification.py
@@ -4,12 +4,12 @@ steps = [("scaler", StandardScaler()),
 pipeline = Pipeline(steps)
 
 # Create the parameter space
-parameters = {"logreg__C": np.linspace(0.001, 1.0, 20)}
+parameter = {"logreg__C": np.linspace(0.001, 1.0, 20)}
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
                                                     random_state=21)
 
-# Instantiate the grid search object
-cv = GridSearchCV(pipeline, param_grid=parameters)
+# Instantiate the grid search objects
+cv = GridSearchCV(pipeline, param_grid=parameter)
 
 # Fit to the training data
 cv.fit(X_train, y_train)
